c953d8a916491de56cb15de9f87a547af0e6fb32
Vardanyan Sargis <143796672+varuniv@users.noreply.github.com>
2025-12-11 10:06:34 +0100

    Delete Prog3D directory

--- Prog3D/TDDepart/b/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/iut45/Etudiants/o22306517/Bureau/BUT3/Prog3D/TDDepart
-# Build directory: /home/iut45/Etudiants/o22306517/Bureau/BUT3/Prog3D/TDDepart/b
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[SimpleTest]=] "td")
-set_tests_properties([=[SimpleTest]=] PROPERTIES  _BACKTRACE_TRIPLES "/home/iut45/Etudiants/o22306517/Bureau/BUT3/Prog3D/TDDepart/CMakeLists.txt;71;add_test;/home/iut45/Etudiants/o22306517/Bureau/BUT3/Prog3D/TDDepart/CMakeLists.txt;0;")
--- Prog3D/canevasTD5/b/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/iut45/Etudiants/o22306517/Bureau/BUT3/Prog3D/canevasTD5
-# Build directory: /home/iut45/Etudiants/o22306517/Bureau/BUT3/Prog3D/canevasTD5/b
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[SimpleTest]=] "td")
-set_tests_properties([=[SimpleTest]=] PROPERTIES  _BACKTRACE_TRIPLES "/home/iut45/Etudiants/o22306517/Bureau/BUT3/Prog3D/canevasTD5/CMakeLists.txt;71;add_test;/home/iut45/Etudiants/o22306517/Bureau/BUT3/Prog3D/canevasTD5/CMakeLists.txt;0;")
