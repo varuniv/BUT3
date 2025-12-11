ecd6e668cbe0799668a3683a904f04184477bfbf
varuniv <sargis.vardanyan@etu.univ-orleans.fr>
2025-12-11 10:58:18 +0100

    11 dec fin de h de tp

--- dithering/src/main.rs
+++ dithering/src/main.rs
@@ -8,7 +8,7 @@ const BLACK: image::Rgb<u8> = image::Rgb([0, 0, 0]);
 fn main() -> Result<(), ImageError> {
     println!("Hello, world!");
     let mut img = ImageReader::open("iut.jpg")?.decode()?.into_rgb8();
-    dithering(&mut img);
+    ordered_dithering(&mut img);
     img.save("out.png")?;
 
     Ok(())
@@ -37,4 +37,22 @@ fn dithering(img : &mut RgbImage) {
         let seuil:u8 = num;
         *pixel = if luminosite[0] > seuil { WHITE } else { BLACK };
     }
+}
+
+fn ordered_dithering(img : &mut RgbImage) {
+    for (x, y, pixel) in img.enumerate_pixels_mut() {
+        let luminosite = pixel.to_luma_alpha();
+        let seuil:u8 = bayer_matrix(x, y);
+        *pixel = if luminosite[0] > seuil { WHITE } else { BLACK };
+    }
+}
+
+fn bayer_matrix(x: u32, y: u32) -> u8 {
+    let bayer = [
+        [0,  8,  2, 10],
+        [12, 4, 14, 6],
+        [3, 11, 1, 9],
+        [15, 7, 13, 5],
+    ];
+    bayer[(y % 4) as usize][(x % 4) as usize] * 16
 }
